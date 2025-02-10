426b929c23cd7e9d71b89eb910e5ef715ddf82a0
Kompukter Reborn 3.0 <afanasevGN@it.mos.ru>
2025-02-10 19:42:40 +0300

    lesson 2 finished

--- src/Lesson2/PrimitiveDataTypes.java
+++ src/Lesson2/PrimitiveDataTypes.java
@@ -17,5 +17,45 @@ public class PrimitiveDataTypes {
         long l2 = 10000000000L;
         long l3 = 52L;
 
+        float f1 = 3.14f;
+        float f2 = 2.5f;
+        float f3 = 20.0f;
+
+        double d1 = 5.5d;
+        double d2 = 87.65d;
+
+        char c1 = 'a';
+        char c2 = 'ф';
+        char c3 = '7';
+        char c4 = ' ';
+
+        char c5 = 500;
+
+        char c6 = '\u00ab';
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+
+
+        int a1 = 60;
+
+        // двоичный вид
+        int a2 = 0B111100;
+
+        // восьмиичный вид
+        int a4 = 0_74;
+
+        // шестнадцатиичный вид
+
+        int a5 = 0x3C;
+        int a6 = 0X3c;
+
+
+
+        int a7 = 1_000_000;
+
+        System.out.println(a7);
+
     }
 }
