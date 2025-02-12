9d92d46de1c1bb9ec186929db6e34cceaeb8f91b
Kompukter Reborn 3.0 <afanasevGN@it.mos.ru>
2025-02-12 22:21:01 +0300

    lesson 3 finished

---
+++ src/Lesson3/Test3.java
@@ -0,0 +1,15 @@
+package Lesson3;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a * 2;
+
+        int x = 1, y = 2, z = 3;
+        var result = x + y + z;
+        System.out.println(result);
+
+        System.out.println(b);
+
+    }
+}
---
+++ src/Lesson3/Test6.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+public class Test6 {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+        int c = 5;
+
+        boolean b3 = a != c;
+
+        boolean x = false ;
+        boolean y = false;
+        boolean z = false;
+
+        boolean result = x || y || z;
+
+        System.out.println(result);
+
+    }
+}
