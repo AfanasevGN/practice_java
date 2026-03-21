60040cab25a4878dc15706033d845d859a8b4813
Gleb <siglebson@gmail.com>
2026-03-21 12:23:13 +0300

    lesson 21 finished

---
+++ src/Lesson21/Test4.java
@@ -0,0 +1,16 @@
+package Lesson21;
+
+public class Test4 {
+  public void abc (String s) {
+    System.out.println(s);
+  }
+
+  public void abc (Boolean b) {
+    System.out.println(b);
+  }
+
+  private StringBuilder abc (StringBuilder sb) {
+    System.out.println(sb);
+    return new StringBuilder("poka");
+  }
+}
